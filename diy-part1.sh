f412b274599e2c3cc72747367eace7c1a726a1a5
汤星明 <xmtang@ittx.com.cn>
2024-08-31 11:53:51 +0800

    调整config

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,8 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 #以下插件在 lede/package 目录下执行即可
 #科学上网插件
